76811af7440d206beb483ece3b2c8c8ac47db13f
makimaki99 <main.markanthony.dano@cvsu.edu.ph>
2025-01-13 22:58:37 +0800

    "adding elements"

--- app/Learning.js
+++ app/Learning.js
@@ -13,13 +13,80 @@ import {
 } from 'react-native';
 
 const elements = [
-    { symbol: 'H', name: 'Hydrogen', number: 1, description: 'Hydrogen is the lightest and most abundant element in the universe, playing a crucial role in stars and the water we drink.', trivia: 'Did you know? Hydrogen makes up about 75% of the universe by mass!', image: 'https://www.climatecouncil.org.au/wp-content/uploads/2021/01/hydrogen.png', application: 'Used in fuel cells and rocket fuel.' },
-    { symbol: 'He', name: 'Helium', number: 2, description: 'Helium is a noble gas often used in balloons and deep-sea diving tanks due to its low density and inert properties.', trivia: 'Fun fact: Helium makes your voice sound funny when inhaled!', image: 'https://www.thoughtco.com/thmb/WjJCGpnJuSx3xprsfEgIdwBdoGc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/186450350-56a132cb5f9b58b7d0bcf751.jpg', application: 'Used in MRI scanners and cooling superconductors.' },
-    { symbol: 'Li', name: 'Lithium', number: 3, description: 'Lithium is a soft, silvery metal used in rechargeable batteries, essential for modern technology.', trivia: 'Lithium is so soft you can cut it with a knife!', image: 'https://www.chemistrylearner.com/wp-content/uploads/2018/09/Lithium-Metal.jpg', application: 'Powers smartphones, laptops, and electric cars.' },
-    { symbol: 'Be', name: 'Beryllium', number: 4, description: 'Hydrogen is the lightest and most abundant element in the universe, playing a crucial role in stars and the water we drink.', trivia: 'Did you know? Hydrogen makes up about 75% of the universe by mass!', image: 'https://www.climatecouncil.org.au/wp-content/uploads/2021/01/hydrogen.png', application: 'Used in fuel cells and rocket fuel.' },
-     
-
+    { 
+        symbol: 'H', 
+        name: 'Hydrogen', 
+        number: 1, 
+        description: 'Hydrogen is the lightest and most abundant element in the universe, playing a crucial role in stars and the water we drink.', 
+        trivia: 'Did you know? Hydrogen makes up about 75% of the universe by mass!', 
+        image: 'https://www.climatecouncil.org.au/wp-content/uploads/2021/01/hydrogen.png', 
+        application: 'Used in fuel cells and rocket fuel.' 
+    },
+    { 
+        symbol: 'He', 
+        name: 'Helium', 
+        number: 2, 
+        description: 'Helium is a noble gas often used in balloons and deep-sea diving tanks due to its low density and inert properties.', 
+        trivia: 'Fun fact: Helium makes your voice sound funny when inhaled!', 
+        image: 'https://www.thoughtco.com/thmb/WjJCGpnJuSx3xprsfEgIdwBdoGc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/186450350-56a132cb5f9b58b7d0bcf751.jpg', 
+        application: 'Used in MRI scanners and cooling superconductors.' 
+    },
+    { 
+        symbol: 'Li', 
+        name: 'Lithium', 
+        number: 3, 
+        description: 'Lithium is a soft, silvery metal used in rechargeable batteries, essential for modern technology.', 
+        trivia: 'Lithium is so soft you can cut it with a knife!', 
+        image: 'https://www.chemistrylearner.com/wp-content/uploads/2018/09/Lithium-Metal.jpg', 
+        application: 'Powers smartphones, laptops, and electric cars.' 
+    },
+    { 
+        symbol: 'B', 
+        name: 'Boron', 
+        number: 5, 
+        description: 'Boron is a rare element that is essential for plant growth and has unique properties in high-tech materials.', 
+        trivia: 'Did you know? Boron is used in borosilicate glass, which is highly heat resistant!', 
+        image: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Boron.jpg', 
+        application: 'Used in detergents, semiconductors, and glassmaking.' 
+    },
+    { 
+        symbol: 'C', 
+        name: 'Carbon', 
+        number: 6, 
+        description: 'Carbon is the basis of all known life on Earth, forming the backbone of complex molecules like DNA.', 
+        trivia: 'Carbon exists as diamond, one of the hardest substances, and graphite, one of the softest.', 
+        image: 'https://upload.wikimedia.org/wikipedia/commons/e/e8/Carbon_Sample.jpg', 
+        application: 'Found in fuels, plastics, and advanced materials like graphene.' 
+    },
+    { 
+        symbol: 'N', 
+        name: 'Nitrogen', 
+        number: 7, 
+        description: 'Nitrogen is a major component of the atmosphere, vital for the production of proteins and DNA in living organisms.', 
+        trivia: "Nitrogen makes up about 78% of Earth's atmosphere but is inert in its natural form.", 
+        image: 'https://upload.wikimedia.org/wikipedia/commons/6/65/Liquidnitrogen.jpg', 
+        application: 'Used in fertilizers, cryogenics, and industrial processes.' 
+    },
+    { 
+        symbol: 'O', 
+        name: 'Oxygen', 
+        number: 8, 
+        description: 'Oxygen is essential for respiration in most life forms and is a major component of water.', 
+        trivia: 'Most of the oxygen we breathe is produced by marine phytoplankton.', 
+        image: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Liquid_oxygen_in_a_beaker.jpg', 
+        application: 'Used in medicine, welding, and water purification.' 
+    },
+    { 
+        symbol: 'F', 
+        name: 'Fluorine', 
+        number: 9, 
+        description: 'Fluorine is the most reactive and electronegative element, forming compounds with nearly all other elements.', 
+        trivia: 'Fluorine is used to make Teflon, the nonstick coating on cookware.', 
+        image: 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Fluorine_Sample.jpg', 
+        application: 'Used in toothpaste, refrigerants, and nonstick coatings.' 
+    }
 ];
+
 const colors = ['#FFCDD2', '#F8BBD0', '#E1BEE7', '#D1C4E9', '#C5CAE9', '#BBDEFB', '#B3E5FC', '#B2EBF2', '#B2DFDB', '#C8E6C9', '#DCEDC8', '#F0F4C3', '#FFECB3', '#FFE0B2', '#FFCCBC', '#D7CCC8', '#CFD8DC'];
 
 
