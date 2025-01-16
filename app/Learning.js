4e7d141b557d4a7df754c1ad243470561688e937
makimaki99 <main.markanthony.dano@cvsu.edu.ph>
2025-01-16 22:58:42 +0800

    add elements, applying code for screen orientation

--- app/Learning.js
+++ app/Learning.js
@@ -10,6 +10,7 @@ import {
     TextInput,
     Alert,
     ScrollView,
+    Dimensions,
 } from 'react-native';
 import styles from './styles';
 const elements = [
@@ -123,6 +124,14 @@ const colors = ['#FFCDD2', '#F8BBD0', '#E1BEE7', '#D1C4E9', '#C5CAE9', '#BBDEFB'
 const Learning = () => {
     const [selectedElement, setSelectedElement] = useState(null);
     const [searchText, setSearchText] = useState('');
+    const [scale, setScale] = useState(1);
+
+    useEffect(() => {
+        ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+        return () => {
+            ScreenOrientation.unlockAsync();
+        };
+    }, []);
 
     const filteredElements = elements.filter(
         (item) =>
